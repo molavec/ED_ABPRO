121afecbc077c472cc366c1fd7cf316daabaad71
Miguel Olave <molavec@gmail.com>
2023-03-22 19:32:12 -0300

    obtiene el roommate

--- index.js
+++ index.js
@@ -6,8 +6,49 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+//TODO: recuperar los roomates desde un archivo
+let roommates = [];
+
+
+app.post('/roommate', (req, res, next)=>{
+  // TODO:
+  fetch('https://randomuser.me/api')
+    .then(resonse => resonse.json())
+    .then(data => {
+      // console.log(data);
+      // 1. Almacenar el roommate en un archivo
+      //TODO:
+      roommates.push(data);
+
+      // 2. retornar el roommate
+      res.send(data);
+    })
+    .catch((error)=> console.log(error));
+});
+
+app.get('/roommates', (req, res, next)=>{
+  // TODO:
+  console.log('roommates', roommates);
+  res.send(roommates);
+});
+
+app.get('/gastos', (req, res, next)=>{
+  // TODO:
+});
+
+app.post('/gastos', (req, res, next)=>{
+  // TODO:
+});
+
+app.put('/gastos', (req, res, next)=>{
+  // TODO:
+});
+
+app.delete('/gastos', (req, res, next)=>{
+  // TODO:
+});
+
 app.get('/', (req, res) => {
-  // res.send('Hello World!')
   res.render('index');
 })
 
